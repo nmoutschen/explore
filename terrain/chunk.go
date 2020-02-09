013d9e94ac99886807fd9f56323f9aee415c95e1
Nicolas Moutschen <nicolas.moutschen@gmail.com>
2020-02-09 18:19:20 +0100

    terrain: added conversion of chunk data to byte array

--- terrain/chunk.go
+++ terrain/chunk.go
@@ -2,17 +2,10 @@ package terrain
 
 //Chunk represents a 32x32 chunk of game world
 type Chunk struct {
-	X        int32        "json:x"
-	Y        int32        "json:y"
-	Data     [32][32]Tile "json:data"
-	Modified bool         "json:modified"
-}
-
-//ID returns a unique identifier for the chunk
-func (c *Chunk) ID() int64 {
-	//Stores c.Y in the 32 most significant bits and c.X in the 32 least significant bits.
-	//This can then be used as a unique identifier for each chunk.
-	return int64(c.Y)<<32 | int64(c.X)
+	X        int32
+	Y        int32
+	Data     [32][32]Tile
+	Modified bool
 }
 
 //NewChunk generates a new chunk using the default generator
@@ -28,4 +21,28 @@ func NewChunkFromData(x int32, y int32, data [32][32]Tile, modified bool) *Chunk
 		Data:     data,
 		Modified: modified,
 	}
+}
+
+//ID returns a unique identifier for the chunk
+func (c *Chunk) ID() uint64 {
+	//Stores c.Y in the 32 most significant bits and c.X in the 32 least significant bits.
+	//This can then be used as a unique identifier for each chunk.
+	x, y := uint32(c.X), uint32(c.Y)
+	return uint64(y)<<32 | uint64(x)
+}
+
+//RawData returns the data as an array of bytes
+func (c *Chunk) RawData() (val [32 * 32]byte) {
+	for xi := 0; xi < 32; xi++ {
+		for yi := 0; yi < 32; yi++ {
+			val[xi*32+yi] = byte(c.Data[xi][yi])
+		}
+	}
+
+	return val
+}
+
+//ChunkIDToPos map a chunk ID into x, y coordinates
+func ChunkIDToPos(id uint64) (x, y int32) {
+	return int32(id & 0xFFFFFFFF), int32(id >> 32)
 }
--- terrain/chunk_test.go
+++ terrain/chunk_test.go
@@ -61,26 +61,42 @@ func ExampleNewChunk() {
 	//[3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3]
 }
 
-func TestChunkID(t *testing.T) {
-	c := Chunk{
-		X: 1,
-		Y: 2,
-	}
-
-	if c.ID() != 0x0000000200000001 {
-		t.Errorf("c.ID() = %x; want %x", c.ID(), 0x0000000200000001)
-	}
-}
-
 func TestNewChunk(t *testing.T) {
-	var x, y int32
-	chunk := NewChunk(x, y)
-
-	if chunk.X != x {
-		t.Errorf("chunk.X = %x; want %x", chunk.X, x)
+	testCases := []struct {
+		X, Y int32
+		ID   uint64
+	}{
+		{0x00000000, 0x00000000, 0x0000_0000_0000_0000},
+		//+1
+		{0x00000001, 0x00000000, 0x0000_0000_0000_0001},
+		{0x00000000, 0x00000001, 0x0000_0001_0000_0000},
+		{0x00000001, 0x00000001, 0x0000_0001_0000_0001},
+		//-1
+		{-0x00000001, 0x00000000, 0x0000_0000_FFFF_FFFF},
+		{0x00000000, -0x00000001, 0xFFFF_FFFF_0000_0000},
+		{-0x00000001, -0x00000001, 0xFFFF_FFFF_FFFF_FFFF},
+		//Max int32
+		{0x7FFFFFFF, 0x00000000, 0x0000_0000_7FFF_FFFF},
+		{0x00000000, 0x7FFFFFFF, 0x7FFF_FFFF_0000_0000},
+		{0x7FFFFFFF, 0x7FFFFFFF, 0x7FFF_FFFF_7FFF_FFFF},
+		//Min int32
+		{-0x80000000, 0x00000000, 0x0000_0000_8000_0000},
+		{0x00000000, -0x80000000, 0x8000_0000_0000_0000},
+		{-0x80000000, -0x80000000, 0x8000_0000_8000_0000},
 	}
-	if chunk.Y != y {
-		t.Errorf("chunk.Y = %x; want %x", chunk.Y, y)
+
+	for i, tc := range testCases {
+		chunk := NewChunk(tc.X, tc.Y)
+
+		if chunk.X != tc.X {
+			t.Errorf("chunk.X = %08x; want %08x for test case %d", chunk.X, tc.X, i)
+		}
+		if chunk.Y != tc.Y {
+			t.Errorf("chunk.Y = %08x; want %08x for test case %d", chunk.Y, tc.Y, i)
+		}
+		if chunk.ID() != tc.ID {
+			t.Errorf("chunk.ID() = %016x; want %016x for test case %d", chunk.ID(), tc.ID, i)
+		}
 	}
 }
 
@@ -103,5 +119,62 @@ func TestNewChunkFromData(t *testing.T) {
 	}
 	if c.Modified != modified {
 		t.Errorf("c.Modified = %t; want %t", c.Modified, modified)
+	}
+}
+
+func TestChunkID(t *testing.T) {
+	c := Chunk{
+		X: 1,
+		Y: 2,
+	}
+
+	if c.ID() != 0x0000000200000001 {
+		t.Errorf("c.ID() = %x; want %x", c.ID(), 0x0000000200000001)
+	}
+}
+
+func TestChunkRawData(t *testing.T) {
+	c := NewChunk(0, 0)
+
+	raw := c.RawData()
+
+	for xi := 0; xi < 32; xi++ {
+		for yi := 0; yi < 32; yi++ {
+			if byte(c.Data[xi][yi]) != raw[xi*32+yi] {
+				t.Errorf("raw[%d] = %x; want %x", xi*32+yi, raw[xi*32+yi], c.Data[xi][yi])
+			}
+		}
+	}
+}
+
+func TestChunkIDToPos(t *testing.T) {
+	testCases := []struct {
+		X, Y int32
+		ID   uint64
+	}{
+		{0x00000000, 0x00000000, 0x0000_0000_0000_0000},
+		//+1
+		{0x00000001, 0x00000000, 0x0000_0000_0000_0001},
+		{0x00000000, 0x00000001, 0x0000_0001_0000_0000},
+		{0x00000001, 0x00000001, 0x0000_0001_0000_0001},
+		//-1
+		{-0x00000001, 0x00000000, 0x0000_0000_FFFF_FFFF},
+		{0x00000000, -0x00000001, 0xFFFF_FFFF_0000_0000},
+		{-0x00000001, -0x00000001, 0xFFFF_FFFF_FFFF_FFFF},
+		//Max int32
+		{0x7FFFFFFF, 0x00000000, 0x0000_0000_7FFF_FFFF},
+		{0x00000000, 0x7FFFFFFF, 0x7FFF_FFFF_0000_0000},
+		{0x7FFFFFFF, 0x7FFFFFFF, 0x7FFF_FFFF_7FFF_FFFF},
+		//Min int32
+		{-0x80000000, 0x00000000, 0x0000_0000_8000_0000},
+		{0x00000000, -0x80000000, 0x8000_0000_0000_0000},
+		{-0x80000000, -0x80000000, 0x8000_0000_8000_0000},
+	}
+
+	for i, tc := range testCases {
+		x, y := ChunkIDToPos(tc.ID)
+		if tc.X != x || tc.Y != y {
+			t.Errorf("ChunkIDToPos(%016x) = (%08x, %08x); want (%08x, %08x) for test case %d", tc.ID, x, y, tc.X, tc.Y, i)
+		}
 	}
 }
